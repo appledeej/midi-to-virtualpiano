5d412690274433d3b1a56b504358886c47cd9ae8
Ben <sendbenspam@yahoo.co.uk>
2019-03-13 12:55:35 +0000

    optimisations

--- log.py
+++ log.py
@@ -8,8 +8,11 @@ def make_timestamp():
 log_file = os.path.join(os.path.dirname(__file__), "logs\\" + make_timestamp() + ".log")
 
 def write_to_log(message):
-    with open(log_file, "a") as f:
-        f.write(make_timestamp() + "/" + message + "\n")
+    if config.write_logs:
+        with open(log_file, "a") as f:
+            f.write(make_timestamp() + "/" + message + "\n")
+    else:
+        pass
 
 def success(message):
     print("[*] " + message)
